d4252a9212bf576992f3cca1e5a71cbc5869c453
Kevin Penney <kpenney@gmail.com>
2020-07-08 09:27:08 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,206 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { title } = require('process');
+
+const app = express()
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// get invoice list
+app.get('/api/invoices', (req, res) => {
+    fs.readFile('UserData.json', (err, data) => {
+        if (err) throw err
+        stuff = JSON.parse(data)
+        res.json(stuff.invoices)
+    })
+})
+
+// get customer list
+app.get('/api/customers', (req, res) => {
+    fs.readFile('UserData.json', (err, data) => {
+        if (err) throw err
+        stuff = JSON.parse(data)
+        res.json(stuff.customers)
+    })
+})
+
+// get single invoice
+app.get('/api/invoices/:id', (req, res) => {
+    fs.readFile('UserData.json', (err, data) => {
+        if (err) throw err
+        data = JSON.parse(data)
+        oneInvoice = data.invoices.filter(invoice => invoice.id === parseInt(req.params.id))
+        res.json(oneInvoice[0])
+    })
+})
+
+// get templates
+app.get('/api/templates', (req, res) => {
+    fs.readFile('UserData.json', (err, data) => {
+        if (err) throw err
+        data = JSON.parse(data)
+        res.json(data.templates)
+    })
+})
+
+// drag and drop reorder line items
+app.patch('/api/invoices/:id', (req, res) => {
+    fs.readFile('UserData.json', (err, data) => {
+        if (err) throw err
+        data = JSON.parse(data)
+        oneInvoice = data.invoices.filter(invoice => invoice.id === parseInt(req.params.id))
+        lineItems = oneInvoice[0].lineItems
+
+        const { oldIndex, newIndex } = req.body
+
+        draggedItem = (lineItems.slice(oldIndex, oldIndex + 1))[0]
+        lineItems.splice(oldIndex, 1)
+        lineItems.splice(newIndex, 0, draggedItem)
+
+        fs.writeFile("UserData.json", JSON.stringify(data, null, 2), err => {
+            if (err) throw err
+            res.json(oneInvoice[0])
+        })
+    })
+})
+
+// Add new template line item
+app.post('/api/templates', (req, res) => {
+    fs.readFile('UserData.json', (err, data) => {
+        if (err) throw err
+        data = JSON.parse(data)
+
+        const { title, description, quantity, unitPrice } = req.body
+        const newTemplateItem = {
+            title,
+            description,
+            quantity,
+            unitPrice
+        }
+        data.templates.push(newTemplateItem)
+
+        fs.writeFile("UserData.json", JSON.stringify(data, null, 2), err => {
+            if (err) throw err
+            res.json(data.templates)
+        })
+    })
+})
+
+// Add new line item
+app.post('/api/invoices/:id', (req, res) => {
+    fs.readFile('UserData.json', (err, data) => {
+        if (err) throw err
+        data = JSON.parse(data)
+        const oneInvoice = data.invoices.filter(invoice => invoice.id === parseInt(req.params.id))[0]
+        const { lineItems } = oneInvoice
+        const { title, description, quantity, unitPrice, amount } = req.body
+
+        const newLineItem = {
+            title,
+            description,
+            quantity,
+            unitPrice,
+            "amount": quantity * unitPrice
+        }
+        lineItems.push(newLineItem)
+
+        oneInvoice.total = lineItems.reduce((total, item) => +total + item.amount, 0)
+
+        fs.writeFile("UserData.json", JSON.stringify(data, null, 2), err => {
+            if (err) throw err
+            res.json(oneInvoice)
+        })
+    })
+})
+
+// Add a new invoice
+app.post('/api/invoices', (req, res) => {
+    fs.readFile('UserData.json', (err, data) => {
+        if (err) throw err
+        data = JSON.parse(data)
+
+        const { customer, daysUntilDue } = req.body
+        var dueDate = new Date()
+        const today = new Date()
+        dueDate.setDate(dueDate.getDate() + +daysUntilDue)
+
+        const newInvoice = {
+            "id": ++data.numberofInvoices,
+            customer,
+            "total": 0,
+            "date": {
+                "month": today.getMonth(),
+                "day": today.getDate(),
+                "year": today.getFullYear()
+            },
+            "dueDate": {
+                "month": dueDate.getMonth(),
+                "day": dueDate.getDate(),
+                "year": dueDate.getFullYear()
+            },
+            "sent": false,
+            "paid": false,
+            "lineItems": []
+        }
+        data.invoices.unshift(newInvoice)
+
+        fs.writeFile("UserData.json", JSON.stringify(data, null, 2), err => {
+            if (err) throw err
+            res.json(data.numberofInvoices)
+        })
+    })
+})
+
+// Edit line item
+app.put('/api/invoices/:id', (req, res) => {
+    fs.readFile('UserData.json', (err, data) => {
+        if (err) throw err
+        data = JSON.parse(data)
+        oneInvoice = data.invoices.filter(invoice => invoice.id === parseInt(req.params.id))[0]
+        const { lineItems } = oneInvoice
+        const { id, title, description, unitPrice, quantity } = req.body
+
+        const newLineItem = {
+            title,
+            description,
+            quantity,
+            unitPrice,
+            "amount": quantity * unitPrice
+        }
+        lineItems[id] = newLineItem
+
+        oneInvoice.total = lineItems.reduce((total, item) => +total + item.amount, 0)
+
+        fs.writeFile("UserData.json", JSON.stringify(data, null, 2), err => {
+            if (err) throw err
+            res.json(oneInvoice)
+        })
+    })
+})
+
+// delete line item
+app.delete('/api/invoices/:id', (req, res) => {
+    fs.readFile('UserData.json', (err, data) => {
+        if (err) throw err
+        data = JSON.parse(data)
+        oneInvoice = data.invoices.filter(invoice => invoice.id === parseInt(req.params.id))[0]
+        const { lineItems } = oneInvoice
+
+        lineItems.splice(req.body.index, 1)
+
+        oneInvoice.total = lineItems.reduce((total, item) => +total + item.amount, 0)
+
+        fs.writeFile("UserData.json", JSON.stringify(data, null, 2), err => {
+            if (err) throw err
+            res.json(oneInvoice)
+        })
+    })
+})
+
+app.use("/", express.static(path.join(__dirname, 'public')))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
---
+++ public/app.js
@@ -0,0 +1,130 @@
+const invoiceTable = document.querySelector('#invoice-list tbody')
+const customerDataList = document.querySelector('#customer-list')
+const streetInput = document.querySelector('#street')
+const cityStateZipInput = document.querySelector('#city-state-zip')
+const customerInput = document.querySelector('#customer')
+const createInvoiceBtn = document.querySelector('#create-invoice-btn')
+const dueInput = document.querySelector('#due')
+const outstandingInvoices = document.querySelector('#outstanding-invoices')
+const outstandingTotal = document.querySelector('#outstanding-total')
+const ytdIncome = document.querySelector('#year-to-date-income')
+const lastYtdIncome = document.querySelector('#last-year-income')
+
+let invoiceList = null
+let customerList = null
+
+fetch('/api/invoices')
+    .then(res => res.json())
+    .then(fillData)
+
+fetch('/api/customers')
+    .then(res => res.json())
+    .then(fillcustomerDataList)
+
+createInvoiceBtn.addEventListener('click', createInvoice)
+
+function createInvoice(e) {
+    e.preventDefault()
+    fetch('/api/invoices', {
+        method: 'POST',
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({
+            "customer": {
+                "name": customerInput.value,
+                "address": {
+                    "street": streetInput.value,
+                    "cityStateZip": cityStateZipInput.value
+                }
+            },
+            "daysUntilDue": dueInput.value
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            sessionStorage.setItem('id', data)
+            window.location = './invoice'
+        })
+}
+
+function fillcustomerDataList(customers) {
+    customerList = customers
+    customerDataList.innerHTML = ''
+    customers.forEach(customer => {
+        customerOption = document.createElement('option')
+        customerOption.value = customer.name
+        customerDataList.appendChild(customerOption)
+    })
+}
+
+function custInput() {
+    customerList.forEach(customer => {
+        if (customerInput.value === customer.name) {
+            streetInput.value = customer.address.street
+            cityStateZipInput.value = customer.address.cityStateZip
+        }
+    })
+}
+
+// Populate invoice table
+function fillData(invoiceList) {
+    let outstandingTotalCount = 0
+    let outstandingTotalAmount = 0
+    let ytdIncomeAmount = 0
+    let lastYtdIncomeAmount = 0
+
+
+    const today = new Date()
+
+    invoiceList.forEach(invoice => {
+        if ( invoice.payment.paid === false ) {
+            outstandingTotalCount++
+            outstandingTotalAmount += invoice.total
+        }
+
+        if ( invoice.payment.date.year === today.getFullYear() ) {
+            ytdIncomeAmount += invoice.total
+        } else if ( invoice.payment.date.year === today.getFullYear() -1 ) {
+            lastYtdIncomeAmount += invoice.total
+        }
+
+        const invoiceRow = document.createElement('tr')
+
+        const id = document.createElement('td')
+        id.textContent = invoice.id
+        invoiceRow.appendChild(id)
+
+        const customer = document.createElement('td')
+        customer.textContent = invoice.customer.name
+        invoiceRow.appendChild(customer)
+
+        const total = document.createElement('td')
+        total.textContent = invoice.total.toFixed(2)
+        total.classList.add('dolla-sign')
+        invoiceRow.appendChild(total)
+
+        const date = document.createElement('td')
+        date.textContent = `${invoice.date.month}/${invoice.date.day}/${invoice.date.year}`
+        invoiceRow.appendChild(date)
+
+        const dueDate = document.createElement('td')
+        dueDate.textContent = `${invoice.dueDate.month}/${invoice.dueDate.day}/${invoice.dueDate.year}`
+        invoiceRow.appendChild(dueDate)
+
+        const paid = document.createElement('td')
+        paid.textContent = invoice.payment.paid === true ? 'Paid' : 'Outstanding'
+        invoiceRow.appendChild(paid)
+
+        invoiceRow.addEventListener('click', () => {
+            sessionStorage.setItem('id', invoice.id)
+            window.location = './invoice'
+        })
+
+        outstandingInvoices.textContent = outstandingTotalCount
+        outstandingTotal.textContent = outstandingTotalAmount.toFixed(2)
+        ytdIncome.textContent = ytdIncomeAmount.toFixed(2)
+        lastYtdIncome.textContent = lastYtdIncomeAmount.toFixed(2)
+
+        invoiceTable.appendChild(invoiceRow)
+    })
+}
+
