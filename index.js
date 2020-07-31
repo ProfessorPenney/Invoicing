c48cb8f37d5e3fd4f74a20cde3c29089d941a79b
Kevin Penney <kpenney@gmail.com>
2020-07-31 14:42:32 -0700

    puppeteer instead of html-pdf

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const fs = require('fs')
 const session = require('express-session')
 const passport = require('passport')
 const flash = require('connect-flash')
-const pdf = require('html-pdf')
 const compression = require('compression')
 const helmet = require('helmet')
 const { ensureAuthenticated } = require('./config/auth')
-
-const pdfTemplate = require('./documents')
+const puppeteer = require('puppeteer')
 
 const app = express()
 
@@ -366,26 +364,224 @@ app.get('/api/invoices/:id/pdf', (req, res) => {
       let payments = 0
       payment.forEach(pay => (payments += pay.amount))
 
-      const pdfData = {
-         companyName: name,
-         companyInfo,
-         custName: customer.name,
-         custStreet: customer.address.street,
-         custCityStateZip: customer.address.cityStateZip,
-         id,
-         date: `${date.month}/${date.day}/${date.year}`,
-         owed: owed.toFixed(2),
-         dueDate: `${dueDate.month}/${dueDate.day}/${dueDate.year}`,
-         payments: payments.toFixed(2),
-         total: total.toFixed(2),
-         lineItemsHTML: tableItems
-      }
+      const companyName = name
+      const custName = customer.name
+      const custStreet = customer.address.street
+      const custCityStateZip = customer.address.cityStateZip
+      const iDate = `${date.month}/${date.day}/${date.year}`
+      const iOwed = owed.toFixed(2)
+      const iDueDate = `${dueDate.month}/${dueDate.day}/${dueDate.year}`
+      const iPayments = payments.toFixed(2)
+      const iTotal = total.toFixed(2)
+      const lineItemsHTML = tableItems
 
-      pdf.create(pdfTemplate(pdfData), {}).toBuffer((err, buffer) => {
-         if (err) return res.status(404).send(err)
-         res.type('pdf')
-         res.end(buffer, 'binary')
-      })
+      // } // puppeteer pdf
+      ;(async function () {
+         try {
+            const browser = await puppeteer.launch()
+            const page = await browser.newPage()
+
+            await page.setContent(`
+   <!DOCTYPE html>
+   <html>
+      <head>
+         <meta charset="utf-8" />
+         <title>Invoice</title>
+         <style>
+         .container {
+            max-width: 750px;
+            margin: 60px auto auto auto;
+            padding: 30px;
+            font-size: 16px;
+            line-height: 24px;
+            font-family: Arial, Helvetica, sans-serif, 'Helvetica';
+            color: black;
+          }
+          
+          #header {
+            position: relative;
+            min-height: 200px;
+          }
+          
+          #header #invoice-bold {
+            position: absolute;
+            margin-top: 0;
+            right: 0px;
+            top: 0;
+            letter-spacing: 0.5em;
+            color: rgb(20, 136, 230);
+          }
+          
+          #header #top-stats {
+            position: absolute;
+            left: 550px;
+            top: 30px;
+            font-size: large;
+          }
+          #header #top-stats #left {
+             position: relative;
+             top: 0;
+             right: 60px;
+             text-align: right;
+          }
+
+          #header #top-stats #right {
+            position: absolute;
+            top: 0;
+            left: 60px;
+          }
+
+          .total-due {
+            font-size: x-large;
+          }
+          
+          table {
+            position: relative;
+            width: 100%;
+            text-align: right;
+            border-collapse: collapse;
+            font-size: large;
+            margin-top: 40px;
+          }
+
+          table tr {
+             height: 40px;
+          }
+          
+          table th {
+            background: rgb(20, 136, 230);
+            color: white;
+            height: 30px;
+            padding-left: 5px;
+            padding-right: 5px;
+          }
+          
+          table td {
+            border-left: 1px solid rgb(175, 175, 175);
+            padding-left: 5px;
+            padding-right: 5px;
+          }
+          
+          table tr :first-child {
+            border-left: none;
+            text-align: left;
+          }
+          
+          table tr :nth-child(2) {
+            width: 50px;
+          }
+          
+          table tr :nth-child(3) {
+            width: 100px;
+          }
+          
+          table tr :nth-child(4) {
+            width: 100px;
+          }
+          
+          table tr * {
+            border-bottom: 1px solid rgb(175, 175, 175);
+          }
+          
+          #table-total {
+            margin-left: auto;
+            text-align: right;
+            width: 220px;
+            font-size: large;
+          }
+          
+          #table-total span {
+            margin-top: 10px;
+            margin-right: 5px;
+            display: inline-block;
+            width: 100px;
+          }
+         </style>
+      </head>
+      <body>
+      <div class="container">
+         <div id="header">
+            <h1 id="company-name">${companyName}<br /></h1>
+            <p id="company-address">
+               ${companyInfo}
+            </p>
+            <h3 id="customer">
+               Bill to: <br />
+            </h3>
+            <p>
+               ${custName} <br />
+               ${custStreet} <br />
+               ${custCityStateZip}
+            </p>
+            <h1 id="invoice-bold">INVOICE</h1>
+            <div id="top-stats">
+               <div id="left">
+                  <p>
+                     Invoice: <br />
+                  </p>
+                  <p>
+                     Date: <br />
+                  </p>
+                  <p>
+                     Due: <br />
+                  </p>
+                  <p class="total-due">
+                     Total Due: 
+                  </p>
+               </div>
+               <div id="right">
+                  <p>
+                     ${id} <br />
+                  </p>
+                  <p>
+                     ${iDate} <br />
+                  </p>
+                  <p>
+                     ${iDueDate} <br />
+                  </p>
+                  <p class="total-due">
+                     $${iOwed}
+                  </p>
+               </div>
+               
+            </div>
+
+         </div>
+         <table>
+            <tr id="heading">
+               <th>Description</th>
+               <th>Quantity</th>
+               <th>Unit Price</th>
+               <th>Total Price</th>
+            </tr>
+            ${lineItemsHTML}
+         </table>
+         <div id="table-total">
+            <span>Total:</span>
+            <span>$${iTotal}</span>
+            <span>Payments:</span>
+            <span>-$${iPayments}</span>
+            <span>Due:</span>
+            <span>$${iOwed}</span>
+         </div>
+      </div>
+      </body>
+   </html>
+    `)
+            await page.emulateMediaFeatures('screen')
+            const newPDF = await page.pdf({
+               // format: 'letter',
+               printBackground: true
+            })
+
+            console.log('done')
+            await browser.close()
+            res.type('pdf')
+            res.end(newPDF, 'binary')
+         } catch (e) {
+            console.log('our error', e)
+         }
+      })()
    })
 })
 
