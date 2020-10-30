6da0677719035897fe709c60c4f35c066b9ec05e
Kevin Penney <kpenney@gmail.com>
2020-10-30 14:16:48 -0700

    questionable merge..

--- App.js
+++ App.js
@@ -37,6 +37,15 @@ app.use(flash())
 // Compression
 app.use(compression())
 
+<<<<<<< HEAD
+=======
+//connect to DB
+mongoose.connect(
+   'mongodb+srv://dbuser:I3vrWKNANaRREn8x@cluster0.trsqi.mongodb.net/test?retryWrites=true&w=majority',
+   () => console.log('connected to DB!')
+)
+
+>>>>>>> mongodb
 // Authentification Route
 app.use('/users', require('./routes/users'))
 
@@ -51,8 +60,11 @@ app.get('/api/customers', (req, res) => {
    res.json(req.user.customers)
 })
 
+<<<<<<< HEAD
 app.use('./api/invoices/:id/pdf', require('./routes/pdf'))
 
+=======
+>>>>>>> mongodb
 app.use('/login', express.static(`${__dirname}/public/login`))
 
 app.use('/register', express.static(`${__dirname}/public/register`))
